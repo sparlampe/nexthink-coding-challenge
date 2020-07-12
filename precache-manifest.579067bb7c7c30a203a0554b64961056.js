6c581d2196afe3cb73e1fa991f7aaa0d249adc80
Roman Babenko <r.babenko@ametiq.com>
2020-07-12 17:25:48 +0200

    Updates

---
+++ precache-manifest.579067bb7c7c30a203a0554b64961056.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9768eec74c0b55802d78194eac37b6e3",
+    "url": "/nexthink-coding-challenge/index.html"
+  },
+  {
+    "revision": "e5d9deb32ca15a9ef6ae",
+    "url": "/nexthink-coding-challenge/static/css/main.00e28358.chunk.css"
+  },
+  {
+    "revision": "af1b8c236efc8510cfdf",
+    "url": "/nexthink-coding-challenge/static/js/2.1f4c4a78.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/nexthink-coding-challenge/static/js/2.1f4c4a78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5d9deb32ca15a9ef6ae",
+    "url": "/nexthink-coding-challenge/static/js/main.51a0bff1.chunk.js"
+  },
+  {
+    "revision": "acfacda1b486e797aece",
+    "url": "/nexthink-coding-challenge/static/js/runtime-main.3ef9c389.js"
+  }
+]);
